e8f2176ac6baac492de98b60b7a12a8ea65f6698
prmika <miika.kauppinen@tuni.fi>
2023-03-25 15:34:26 +0200

    Added few frontend tests
    Added couple of tests for Cities List and CitiesItem

--- frontend/src/cities/components/CitiesList.jsx
+++ frontend/src/cities/components/CitiesList.jsx
@@ -3,6 +3,14 @@ import React from "react";
 import CityItem from './CitiesItem';
 import './CitiesList.css';
 const CitiesList = props => {
+
+if (props.items.length === 0) {
+  return (
+    <div className="center">
+      <h2>No cities found.</h2>
+    </div>
+  );
+}
   return <ul className="cities-list">
     {props.items.map(city =>
       <CityItem
